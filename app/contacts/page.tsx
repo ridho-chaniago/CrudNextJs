2af1a78cf570228f3a370b652c324cf2da9d6d32
Ridho-Chaniago <ridho12c.rc@gmail.com>
2024-04-02 11:14:50 +0700

    first commit

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,5 +1,13 @@
-const Contact =()=>{
-    return (
-        <div></div>
-    )
-}
+import ContactTable from "@/components/contact-table";
+import Search from "@/components/Search";
+const Contact = () => {
+  return (
+    <div className="max-w-screen-md mx-auto mt-5">
+      <div className="flex items-center justify-between gap-1 mb-5">
+        <Search />
+      </div>
+      <ContactTable></ContactTable>
+    </div>
+  );
+};
+export default Contact;
